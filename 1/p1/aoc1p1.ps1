a881092b9282309d11db4cfcfa05a4ec77b40852
OstenTV <info@ostentv.dk>
2024-12-01 12:49:16 +0100

    Add day 1

---
+++ 1/p1/aoc1p1.ps1
@@ -0,0 +1,34 @@
+﻿$In = (Get-Content -Path .\in.txt -Raw)
+
+[int]$Result = 0;
+
+$l1 = @()
+$l2 = @()
+
+foreach ($line in $In.Split("`n")){
+    if ($line -eq "") {continue}
+    $Split = $line.Replace("   ",",").Split(",");
+    $l1 += [int]$Split[0]
+    $l2 += [int]$Split[1]
+}
+
+$l1 = $l1 | Sort-Object
+$l2 = $l2 | Sort-Object
+
+for ($i = 0; $i -lt ($l1.Count); $i++)
+{ 
+    $n1 = $l1[$i]
+    $n2 = $l2[$i]
+    if ($n1 -ge $n2) {
+        $d = $n1 - $n2
+    } else {
+        $d = $n2 - $n1
+    }
+
+    $Result += $d;
+
+}
+
+$Result
+
+$Result | Set-Content -Path .\out.txt -Encoding UTF8
---
+++ 1/p2/aoc1p2.ps1
@@ -0,0 +1,37 @@
+﻿$In = (Get-Content -Path .\in.txt -Raw)
+
+[int]$Result = 0;
+
+$l1 = @()
+$l2 = @()
+
+foreach ($line in $In.Split("`n")){
+    if ($line -eq "") {continue}
+    $Split = $line.Replace("   ",",").Split(",");
+    $l1 += [int]$Split[0]
+    $l2 += [int]$Split[1]
+}
+
+$l1 = $l1 | Sort-Object
+$l2 = $l2 | Sort-Object
+
+for ($i = 0; $i -lt ($l1.Count); $i++)
+{ 
+    $n1 = $l1[$i]
+    $c = 0
+
+    foreach ($n2 in $l2) {
+        if ($n1 -eq $n2) {
+            $c++
+        }
+    }
+
+    $d = $n1 * $c
+
+    $Result += $d;
+
+}
+
+$Result
+
+$Result | Set-Content -Path .\out.txt -Encoding UTF8
