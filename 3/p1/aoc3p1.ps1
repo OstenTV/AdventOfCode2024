3625939e78b3e6449052c0088c2d505d0138f867
Tobias Sindrup <info@ostentv.dk>
2024-12-03 08:48:40 +0100

    Add day 3

---
+++ 3/p1/aoc3p1.ps1
@@ -0,0 +1,23 @@
+﻿$In = (Get-Content -Path .\in.txt -Raw)
+
+[int]$Result = 0;
+
+# Define the regex pattern
+$pattern = "mul\((\d+),(\d+)\)"
+
+# Find all matches
+$matches = [regex]::Matches($In, $pattern)
+
+# Output the matches
+foreach ($match in $matches) {
+    [int]$number1 = $match.Groups[1].Value
+    [int]$number2 = $match.Groups[2].Value
+    Write-Output "First number: $number1, Second number: $number2"
+    $n3 = $number1 * $number2;
+
+    $Result += $n3;
+}
+
+$Result
+
+$Result | Set-Content -Path .\out.txt -Encoding UTF8
---
+++ 3/p2/aoc3p2.ps1
@@ -0,0 +1,32 @@
+﻿$In = (Get-Content -Path .\in.txt -Raw)
+
+[int]$Result = 0;
+
+# Define the regex pattern
+$pattern = "mul\((\d+),(\d+)\)|don't\(\)|do\(\)"
+
+# Find all matches
+$matches = [regex]::Matches($In, $pattern)
+
+$do = $true
+
+# Output the matches
+foreach ($match in $matches) {
+    if ( ($do -eq $true) -and ($match.Groups[0].Value -match "mul")) {
+        [int]$number1 = $match.Groups[1].Value
+        [int]$number2 = $match.Groups[2].Value
+        Write-Output "First number: $number1, Second number: $number2"
+        $n3 = $number1 * $number2;
+        $Result += $n3;
+    } elseif ($match.Groups[0].Value -eq "don't()") {
+        Write-Output "Found: don't()"
+        $do = $false
+    } elseif ($match.Groups[0].Value -eq "do()") {
+        Write-Output "Found: do()"
+        $do = $true
+    }
+}
+
+$Result
+
+$Result | Set-Content -Path .\out.txt -Encoding UTF8
