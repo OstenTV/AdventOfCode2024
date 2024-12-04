f8cc56760b3f95f8fe9e9827f893b4a23b236135
Tobias Sindrup <info@ostentv.dk>
2024-12-04 11:48:11 +0100

    Add day 4

---
+++ 4/p1/aoc4p1.ps1
@@ -0,0 +1,78 @@
+﻿$In = (Get-Content -Path .\input.txt -Raw)
+$WordArray = "XMAS".ToCharArray();
+
+[int]$Result = 0;
+
+$rows = $In.Split("`n")
+
+$Map = @()
+for ($x = 0; $x -lt $rows.Length; $x++) { 
+    $row = $rows[$x].ToCharArray()
+    $Map += ,$row
+}
+
+function Check-WordMap ($x, $y, $d, $i) {
+    $ltr = $Map[$x][$y]
+    $SearchLtr = $WordArray[$i]
+    if ($ltr -eq $SearchLtr) {
+        $Count = 0;
+        if ($i -eq ($WordArray.Count)-1) {
+            return 1
+        }
+        $i++;
+
+        if ($d -eq 1 -or $d -eq 0) {
+            if ($x + 1 -lt $Map.Length) {
+                $Count += Check-WordMap -x ($x + 1) -y $y -d 1 -i $i
+            }
+        }
+        if ($d -eq 2 -or $d -eq 0) {
+            if ($x + 1 -lt $Map.Length -and $y + 1 -lt $Map[$x].Length) {
+                $Count += Check-WordMap -x ($x + 1) -y ($y + 1) -d 2 -i $i
+            }
+        }
+        if ($d -eq 3 -or $d -eq 0) {
+            if ($y + 1 -lt $Map[$x].Length) {
+                $Count += Check-WordMap -x $x -y ($y + 1) -d 3 -i $i
+            }
+        }
+        if ($d -eq 4 -or $d -eq 0) {
+            if ($x - 1 -ge 0 -and $y + 1 -lt $Map[$x].Length) {
+                $Count += Check-WordMap -x ($x - 1) -y ($y + 1) -d 4 -i $i
+            }
+        }
+        if ($d -eq 5 -or $d -eq 0) {
+            if ($x - 1 -ge 0) {
+                $Count += Check-WordMap -x ($x - 1) -y $y -d 5 -i $i
+            }
+        }
+        if ($d -eq 6 -or $d -eq 0) {
+            if ($x - 1 -ge 0 -and $y - 1 -ge 0) {
+                $Count += Check-WordMap -x ($x - 1) -y ($y - 1) -d 6 -i $i
+            }
+        }
+        if ($d -eq 7 -or $d -eq 0) {
+            if ($y - 1 -ge 0) {
+                $Count += Check-WordMap -x $x -y ($y - 1) -d 7 -i $i
+            }
+        }
+        if ($d -eq 8 -or $d -eq 0) {
+            if ($x + 1 -lt $Map.Length -and $y - 1 -ge 0) {
+                $Count += Check-WordMap -x ($x + 1) -y ($y - 1) -d 8 -i $i
+            }
+        }
+        return $Count
+    } else {
+        return 0
+    }
+}
+
+for ($x = 0; $x -lt $Map.Length; $x++) {
+    for ($y = 0; $y -lt $Map[$x].Length; $y++) { 
+        $Result += Check-WordMap -x $x -y $y -d 0 -i 0
+    }
+}
+
+
+$Result
+$Result | Set-Content -Path .\out.txt -Encoding UTF8
---
+++ 4/p2/aoc4p2.ps1
@@ -0,0 +1,28 @@
+﻿$In = (Get-Content -Path .\input.txt -Raw)
+
+[int]$Result = 0
+
+# Split the input into rows
+$grid = $In.Split("`n")
+
+for ($i = 0; $i -lt $grid.Length - 3; $i++) {
+    for ($j = 0; $j -lt $grid[$i].Length - 2; $j++) {
+        # Extract 3x3 subgrid
+        $sub1 = $grid[$i].Substring($j, 3)
+        $sub2 = $grid[$i + 1].Substring($j, 3)
+        $sub3 = $grid[$i + 2].Substring($j, 3)
+
+        if ($sub2[1] -eq 'A' -and $sub1[0] -eq 'M' -and $sub1[2] -eq 'M' -and $sub3[0] -eq 'S' -and $sub3[2] -eq 'S') {$Result++}
+        if ($sub2[1] -eq 'A' -and $sub1[0] -eq 'S' -and $sub1[2] -eq 'S' -and $sub3[0] -eq 'M' -and $sub3[2] -eq 'M') {$Result++}
+        if ($sub2[1] -eq 'A' -and $sub1[0] -eq 'S' -and $sub1[2] -eq 'M' -and $sub3[0] -eq 'S' -and $sub3[2] -eq 'M') {$Result++}
+        #if ($sub2[1] -eq 'A' -and $sub1[0] -eq 'S' -and $sub1[2] -eq 'M' -and $sub3[0] -eq 'M' -and $sub3[2] -eq 'S') {$Result++}
+        #if ($sub2[1] -eq 'A' -and $sub1[0] -eq 'M' -and $sub1[2] -eq 'S' -and $sub3[0] -eq 'S' -and $sub3[2] -eq 'M') {$Result++}
+        if ($sub2[1] -eq 'A' -and $sub1[0] -eq 'M' -and $sub1[2] -eq 'S' -and $sub3[0] -eq 'M' -and $sub3[2] -eq 'S') {$Result++}
+
+    }
+
+}
+
+$Result
+
+$Result | Set-Content -Path .\out.txt -Encoding UTF8
